896976e42c9b94feab1c3017cf31e7972831c5cf
Glen Maddern <glenmaddern@gmail.com>
2013-01-28 18:33:58 +1100

    added some tests to cover updating the URL

--- presentation/app/scripts/controllers/main.js
+++ presentation/app/scripts/controllers/main.js
@@ -1,6 +1,11 @@
 'use strict';
 
-presentationApp.controller('MainCtrl', function($scope) {
+presentationApp.controller('MainCtrl', function ($scope, $location) {
   $scope.currentSlide = 1;
-  $scope.awesomeThings = [1,2,3];
+  var setCurrentSlide = function (nr) {
+    var ohGodJavascriptReally = nr.toFixed(1);
+    $location.hash(ohGodJavascriptReally);
+    return $scope.currentSlide = parseFloat(ohGodJavascriptReally);
+  };
+  setCurrentSlide(parseFloat($location.hash()) || 1);
 });
--- presentation/test/spec/controllers/main.js
+++ presentation/test/spec/controllers/main.js
@@ -1,22 +1,38 @@
 'use strict';
 
-describe('Controller: MainCtrl', function() {
+describe('Controller: MainCtrl', function () {
 
   // load the controller's module
   beforeEach(module('presentationApp'));
 
-  var MainCtrl,
-    scope;
+  var scope, location, invokeMainCtrl;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function($controller) {
+  beforeEach(inject(function ($controller) {
     scope = {};
-    MainCtrl = $controller('MainCtrl', {
-      $scope: scope
-    });
+    location = jasmine.createSpyObj('location', ['hash']);
+    invokeMainCtrl = function() {
+      $controller('MainCtrl', { $scope: scope, $location: location });
+    }
   }));
 
-  it('should attach a list of awesomeThings to the scope', function() {
-    expect(scope.awesomeThings.length).toBe(3);
+  it("should default to slide 1.0", function() {
+    invokeMainCtrl();
+    expect(scope.currentSlide).toBe(1.0);
+    expect(location.hash).toHaveBeenCalledWith('1.0');
+  });
+
+  it("should set the currentSlide with what's in the URL", function() {
+    location.hash.andReturn('2.1');
+    invokeMainCtrl();
+    expect(scope.currentSlide).toBe(2.1);
+    expect(location.hash).toHaveBeenCalledWith('2.1');
+  });
+
+  it("should set the currentSlide and the URL to 1 decimal place", function() {
+    location.hash.andReturn('2.300001');
+    invokeMainCtrl();
+    expect(scope.currentSlide).toBe(2.3);
+    expect(location.hash).toHaveBeenCalledWith('2.3');
   });
 });
