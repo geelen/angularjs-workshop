36ef212acbbc6fd1bb5d37d0af4e06c883ab8a16
Glen Maddern <glenmaddern@gmail.com>
2013-01-29 00:57:17 +1100

    Added and tested the snippet too.

---
+++ presentation/app/scripts/directives/snippet.js
@@ -0,0 +1,17 @@
+'use strict';
+
+presentationApp.directive('snippet', function($http) {
+  return {
+    restrict: 'E',
+    replace: true,
+    template: "<pre><code data-language='{{lang}}'>{{content}}</code></pre>",
+    scope: true,
+    link: function(scope, element, attrs) {
+      scope.lang = attrs.lang;
+      $http.get(attrs.file + "?livereload=false").success(function(data) {
+        dump(data);
+        scope.content = data;
+      });
+    }
+  };
+});
