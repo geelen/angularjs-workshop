28b6ddd9b5a304f237cc947182755c541fa8f6a6
Glen Maddern <glenmaddern@gmail.com>
2013-01-28 13:35:11 +1100

    presentation: yeoman init angular

---
+++ presentation/app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var presentationApp = angular.module('presentationApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
