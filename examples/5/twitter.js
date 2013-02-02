eb2acd9acde22a3d69d33497268a46bf019618fb
Glen Maddern <glenmaddern@gmail.com>
2013-02-03 00:29:50 +1100

    ex5 stage 2

--- examples/5/twitter.js
+++ examples/5/twitter.js
@@ -1,5 +1,8 @@
-var TwitterController = function($scope, $http) {
-  $http.jsonp("http://search.twitter.com/search.json?q=seek&callback=JSON_CALLBACK").success(function(data) {
-    $scope.results = data.results;
-  });
+var TwitterController = function ($scope, $http) {
+  $scope.search = function () {
+    $http.jsonp("http://search.twitter.com/search.json?q=" + $scope.searchTerm + "&callback=JSON_CALLBACK").success(function (data) {
+      $scope.results = data.results;
+      console.log(data);
+    });
+  }
 };
