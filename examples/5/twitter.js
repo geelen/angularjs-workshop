a27f54c97a15642ac634848e9b3f9f4fd432ffe2
Glen Maddern <glenmaddern@gmail.com>
2013-02-03 00:34:02 +1100

    Added finished version to slides

--- examples/5/twitter.js
+++ examples/5/twitter.js
@@ -1,6 +1,10 @@
 var TwitterController = function ($scope, $http) {
   $scope.search = function () {
-    $http.jsonp("http://search.twitter.com/search.json?q=" + $scope.searchTerm + "&callback=JSON_CALLBACK").success(function (data) {
+    $http.jsonp(
+      "http://search.twitter.com/search.json?q=" +
+      $scope.searchTerm +
+      "&callback=JSON_CALLBACK"
+    ).success(function (data) {
       $scope.results = data.results;
     });
   }
