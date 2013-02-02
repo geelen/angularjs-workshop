8a69a093e29548b1ace162bbbe98e88146b8f40e
Glen Maddern <glenmaddern@gmail.com>
2013-02-03 00:32:32 +1100

    Twitter example complete

--- examples/5/twitter.js
+++ examples/5/twitter.js
@@ -2,7 +2,6 @@ var TwitterController = function ($scope, $http) {
   $scope.search = function () {
     $http.jsonp("http://search.twitter.com/search.json?q=" + $scope.searchTerm + "&callback=JSON_CALLBACK").success(function (data) {
       $scope.results = data.results;
-      console.log(data);
     });
   }
 };
