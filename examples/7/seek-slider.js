30b12367bc32463a7c90455e153c8bd4880e4f23
Glen Maddern <glenmaddern@gmail.com>
2013-02-03 10:37:08 +1100

    deciding to bail on this

--- examples/7/seek-slider.js
+++ examples/7/seek-slider.js
@@ -1,4 +1,25 @@
 var app = angular.module('seekSlider', []);
 
 app.controller('SeekController', function ($scope) {
+  $scope.minSalary = 0;
+  $scope.maxSalary = 200;
+});
+
+app.directive('grabMe', function() {
+  return function(scope, element, attr) {
+    var originalX, dragging = false;
+      window.fullWidth = element;
+    console.log(element)
+    element.bind('mousedown', function(e) {
+      dragging = true;
+      originalX = e.offsetX;
+    });
+    element.bind('mousemove', function(e) {
+      if (dragging)
+        console.log(e.offsetX - originalX);
+    });
+    element.bind('mouseup', function() {
+      dragging = false;
+    });
+  }
 });
