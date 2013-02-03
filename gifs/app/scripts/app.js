c5b147aa7a61807b11d471cb3e805a1350d8017f
Glen Maddern <glenmaddern@gmail.com>
2013-02-03 19:05:52 +1100

    Switching to a non ng-view layout

--- gifs/app/scripts/app.js
+++ gifs/app/scripts/app.js
@@ -1,13 +1,3 @@
 'use strict';
 
-var gifsApp = angular.module('gifsApp', [])
-  .config(['$routeProvider', function($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainController'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  }]);
+var gifsApp = angular.module('gifsApp', []);
