97215e9e2716f524191783decf2ee708ba314c35
Glen Maddern <glenmaddern@gmail.com>
2013-02-04 09:21:17 +1100

    added the demo presentation

---
+++ demo_preso/app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var demoPresoApp = angular.module('demoPresoApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
