42a42834ef9203d35d8b26292b10d99207f1d74d
Glen Maddern <glenmaddern@gmail.com>
2013-02-03 12:18:43 +1100

    changing the base route to /slides

--- presentation/app/scripts/app.js
+++ presentation/app/scripts/app.js
@@ -3,12 +3,12 @@
 var presentationApp = angular.module('presentationApp', [])
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider
-      .when('/', {
+      .when('/slides', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl',
         reloadOnSearch: false
       })
       .otherwise({
-        redirectTo: '/'
+        redirectTo: '/slides'
       });
   }]);
