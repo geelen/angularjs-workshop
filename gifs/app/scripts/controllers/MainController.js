b7ce133283953a339d994e90a3aeb0ee3504421c
Glen Maddern <glenmaddern@gmail.com>
2013-02-03 18:23:52 +1100

    Got ourselves a big gif display

--- gifs/app/scripts/controllers/MainController.js
+++ gifs/app/scripts/controllers/MainController.js
@@ -1,6 +1,9 @@
 'use strict';
 
 gifsApp.controller('MainController', function ($scope, $http) {
+
+  $scope.currentGif = "http://24.media.tumblr.com/437d0b5ebeb039d48ef586c11189b781/tumblr_mgzzfund6t1qzt4vjo1_r1_500.gif";
+
   $http.jsonp('http://api.tumblr.com/v2/blog/dvdp.tumblr.com/posts?api_key=MzOPX3SNf2sIvdWg2DQvdaDGbw8VbfRz3vPeImWwvBCJ2Q7fzn&callback=JSON_CALLBACK').success(function (data) {
     var gifs = [];
 
