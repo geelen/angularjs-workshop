5f331294eb39417e702b90a03445b0b4569b2299
Glen Maddern <glenmaddern@gmail.com>
2013-02-03 18:47:13 +1100

    Added a spinner

--- gifs/app/scripts/controllers/MainController.js
+++ gifs/app/scripts/controllers/MainController.js
@@ -1,9 +1,15 @@
 'use strict';
 
 gifsApp.controller('MainController', function ($scope, $http, Fetcher) {
+  var allGifs = ['http://project.wnyc.org/elections/patchwork-vote-co/images/spinner.gif'];
+
+  var chooseRandomGif = function() {
+    $scope.currentGif = allGifs[Math.floor(Math.random() * allGifs.length)]
+  };
+  chooseRandomGif();
 
   Fetcher.fetch().then(function (gifs) {
-    $scope.currentGif = gifs[Math.floor(Math.random() * gifs.length)]
+    allGifs = gifs;
+    chooseRandomGif();
   });
-
 });
