e022954030a3a622d2ea13471ba5c49d2da69e5c
Glen Maddern <glenmaddern@gmail.com>
2013-02-04 13:09:06 +1100

    generated

---
+++ demo/app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var demoApp = angular.module('demoApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ demo/app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+demoApp.controller('MainCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
