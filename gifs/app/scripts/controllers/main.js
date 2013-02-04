622882446d96f678cea0cfa45c0efc14ec868c40
Glen Maddern <glenmaddern@gmail.com>
2013-02-04 15:18:14 +1100

    Fetcher service

--- gifs/app/scripts/controllers/main.js
+++ gifs/app/scripts/controllers/main.js
@@ -1,9 +1,12 @@
 'use strict';
 
-gifsApp.controller('MainCtrl', function($scope) {
-  $scope.awesomeThings = [
-    'HTML5 Boilerplate',
-    'AngularJS',
-    'Testacular'
-  ];
+gifsApp.controller('MainCtrl', function($scope, $http, Fetcher) {
+  $scope.gifs = [];
+
+  Fetcher.fetch('dvdp').then(function (gifs) {
+    $scope.gifs = $scope.gifs.concat(gifs);
+  });
+  Fetcher.fetch('simpsonsgifs').then(function (gifs) {
+    $scope.gifs = $scope.gifs.concat(gifs);
+  });
 });
