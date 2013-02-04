4ffd18c7ce31015ad2197a59f45623a1adbc8ca0
Glen Maddern <glenmaddern@gmail.com>
2013-02-04 14:51:37 +1100

    yeoman init

---
+++ gifs/app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var gifsApp = angular.module('gifsApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
