ec09366591091c2a59e251be8ea9302eaed87942
Glen Maddern <glenmaddern@gmail.com>
2013-02-08 19:41:34 +1100

    Set up the gifs demo to save time.

--- gifs/app/scripts/controllers/main.js
+++ gifs/app/scripts/controllers/main.js
@@ -1,9 +1,18 @@
 'use strict';
 
-gifsApp.controller('MainCtrl', function($scope) {
-  $scope.awesomeThings = [
-    'HTML5 Boilerplate',
-    'AngularJS',
-    'Testacular'
-  ];
+gifsApp.controller('MainCtrl', function ($scope, $http) {
+  $scope.gifs = [];
+
+  $http.jsonp('http://api.tumblr.com/v2/blog/dvdp.tumblr.com/posts?api_key=MzOPX3SNf2sIvdWg2DQvdaDGbw8VbfRz3vPeImWwvBCJ2Q7fzn&callback=JSON_CALLBACK')
+    .success(function (data) {
+      data.response.posts.forEach(function (post) {
+        if (post.photos) {
+          post.photos.forEach(function (photo) {
+            var url = photo.original_size.url;
+            if (url.match(/http[^"]*?\.gif/))
+              $scope.gifs.push(url);
+          });
+        }
+      });
+    });
 });
