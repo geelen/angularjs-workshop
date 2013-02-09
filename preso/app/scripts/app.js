16faaf15ef9d597e889980f2388b2852770d57d2
Glen Maddern <glenmaddern@gmail.com>
2013-02-09 11:34:44 +1100

    generated preso

---
+++ preso/app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var presoApp = angular.module('presoApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
