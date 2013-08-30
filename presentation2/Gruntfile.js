a44aa5410541c9ceab5e11b1fcf6b87edb606148
Glen Maddern <glenmaddern@gmail.com>
2013-08-30 15:40:29 +1000

    Starting to move the info across

--- presentation2/Gruntfile.js
+++ presentation2/Gruntfile.js
@@ -45,7 +45,7 @@ module.exports = function (grunt) {
     },
     connect: {
       options: {
-        port: 9000,
+        port: 9001,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: 'localhost'
       },
@@ -266,7 +266,7 @@ module.exports = function (grunt) {
     'clean:server',
     'coffee:dist',
     'compass:server',
-    'livereload-start',
+//    'livereload-start',
     'connect:livereload',
     'open',
     'watch'
