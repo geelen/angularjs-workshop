2c25072235326afafa1c0e19b5573b398ef5243f
Glen Maddern <glenmaddern@gmail.com>
2013-08-31 18:35:16 +1000

    Added a loading screen

--- github-browser/scripts/services/search.js
+++ github-browser/scripts/services/search.js
@@ -13,9 +13,12 @@
 
     // Function implementations
     Search.go = function () {
+      Search.users = undefined;
       $http.get(githubSearchUrl + Search.term)
         .success(function (response) {
           Search.users = response.users;
+        }).error(function() {
+          Search.users = [];
         });
 
       console.log("Searching...");
