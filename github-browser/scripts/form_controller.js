b00250027161f105dc184c8a6f7d984ef072e27e
Glen Maddern <glenmaddern@gmail.com>
2013-08-31 11:19:42 +1000

    Simple localStorage saving of variables

--- github-browser/scripts/form_controller.js
+++ github-browser/scripts/form_controller.js
@@ -2,9 +2,25 @@
   'use strict';
 
   app.controller("FormController", function ($scope) {
+    // Private state
+    var localStorageBucket = 'github-browser__me.name';
+
+    // Setup tasks
+    if (!$scope.me.name) {
+      $scope.me.name = localStorage.getItem(localStorageBucket)
+    }
+
+    // Watchers
+    $scope.$watch('me.name', function (newVal) {
+      localStorage.setItem(localStorageBucket, newVal);
+    });
+
+    // Public API
     $scope.reset = function () {
       $scope.me.name = "";
     }
   });
 
+
 })(angular.module('GithubBrowser'));
+
