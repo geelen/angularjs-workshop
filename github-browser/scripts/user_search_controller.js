57fa7ef856a3a717258b6293a6a5244c5f84ea2e
Glen Maddern <glenmaddern@gmail.com>
2013-08-31 12:06:55 +1000

    Hitting the Github api for the first time

--- github-browser/scripts/user_search_controller.js
+++ github-browser/scripts/user_search_controller.js
@@ -1,7 +1,16 @@
 (function (app) {
   'use strict';
 
-  app.controller("UserSearchController", function ($scope) {
+  app.controller("UserSearchController", function ($scope, $http) {
+    $scope.search = {
+      go: function() {
+        $http.get("https://api.github.com/legacy/user/search/" + this.term)
+          .success(function(response) {
+            console.log(response.users);
+          })
+        console.log("Searching...");
+      }
+    }
   });
 
 
