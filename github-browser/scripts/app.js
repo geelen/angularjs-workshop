42a9b4a993de96143b1159103a48ea4eff1e63f8
Glen Maddern <glenmaddern@gmail.com>
2013-08-31 18:16:06 +1000

    Adding token auth to all http requests

--- github-browser/scripts/app.js
+++ github-browser/scripts/app.js
@@ -4,6 +4,8 @@
   var app = angular.module("GithubBrowser", [])
 
   // global config goes here
-
+  app.config(function($httpProvider) {
+    $httpProvider.defaults.headers.common.Authorization = "token 8e0e97fe6cfc172a1d640dac677255008e8ee73d";
+  });
 
 })();
