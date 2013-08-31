6c8069ccad564b2af7162a9faa3b7344430a03ca
Glen Maddern <glenmaddern@gmail.com>
2013-08-31 12:22:05 +1000

    Displaying the results ok

--- github-browser/scripts/user_search_controller.js
+++ github-browser/scripts/user_search_controller.js
@@ -5,9 +5,10 @@
     $scope.search = {
       go: function() {
         $http.get("https://api.github.com/legacy/user/search/" + this.term)
-          .success(function(response) {
-            console.log(response.users);
-          })
+          .success(angular.bind(this, function(response) {
+            this.users = response.users;
+          }));
+
         console.log("Searching...");
       }
     }
